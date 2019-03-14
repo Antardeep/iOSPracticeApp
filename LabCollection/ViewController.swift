e9ce2fc0e054bde4d8495bd00aaf84a3da5e5136
Guest User <Guest@macos-20.local>
2019-03-14 16:19:27 -0400

    Initial Commit

---
+++ LabCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabCollection
+//
+//  Created by Guest User on 2019-03-14.
+//  Copyright © 2019 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
